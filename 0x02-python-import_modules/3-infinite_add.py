a19445359f157fdd157615200d933ff92bd6129c
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-05 12:37:22 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+i = 0
+result = 0
+
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
