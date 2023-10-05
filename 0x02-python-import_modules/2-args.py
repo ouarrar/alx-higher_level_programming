3f659de3d375935ec2fa1a4921e61cc60332443c
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-05 12:27:48 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+args = "{:d} argument"
+lenarg = len(sys.argv) - 1
+if lenarg == 0:
+    args += 's.'
+elif lenarg == 1:
+    args += ':'
+else:
+    args += 's:'
+print(args.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
