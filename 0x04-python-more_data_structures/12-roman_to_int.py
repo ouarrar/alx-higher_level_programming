846a995e20c99c00c083b64d7d5cda6d639feabe
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-11 14:56:15 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-def subtract_list(listnum):
-	tosub = 0
-	maxlist = max(listnum)
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
 
-	for n in listnum:
-		if maxlist > n:
-			tosub += n
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
 
-	return (maxlist - tosub)
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
-	if not roman_string:
-		return 0
+    if not roman_string:
+        return 0
 
-	if not isinstance(roman_string, str):
-		return 0
+    if not isinstance(roman_string, str):
+        return 0
 
-	roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-	list_keys = list(roman_num.keys())
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
-	num = 0
-	lastnum = 0
-	listnum = [0]
+    num = 0
+    last_rom = 0
+    list_num = [0]
 
-	for c in roman_string:
-		for k in list_keys:
-			if k == c:
-				if roman_num.get(c) <= lastnum:
-					num += subtract_list(listnum)
-					listnum = [roman_num.get(c)]
-				else:
-					listnum.append(roman_num.get(c))
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
 
-				lastnum = roman_num.get(c)
+                last_rom = rom_n.get(ch)
 
-	num += subtract_list(listnum)
+    num += to_subtract(list_num)
 
-	return (num)
+    return (num)
