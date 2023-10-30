f12084fc091ef6248ac5dcb000ded9d2390e6e38
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-30 16:21:58 -0700

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,9 +6,26 @@ Defines a Rectangle class with attribute and action
 
 class Rectangle:
     """
-    Represents a rectangle
-    """
+    Defines class rectangle with private attribute width and height
 
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        __str__(self)
+        __repr__(self)
+        __del__(self)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+        bigger_or_equal(rect_1, rect_2)
+        square(cls, size=0)
+    """
     number_of_instances = 0
     print_symbol = "#"
 
