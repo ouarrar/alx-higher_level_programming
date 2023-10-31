b4e0949002264176473ea7ef12456eaee2c99b3e
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-10-31 14:26:37 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
@@ -8,21 +7,3 @@ matrix = [
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
-
-
-+++++++++++++++++++++++++++++++++++
-
-3-main.py
-
-#!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
-
-
